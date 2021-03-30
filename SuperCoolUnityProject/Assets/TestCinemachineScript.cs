9562fdfff8b173fb7196bd1375bd6fdd68fed675
DoubleBrackets <arthurgan29@gmail.com>
2021-03-29 23:49:25 -0700

    Parallax

---
+++ SuperCoolUnityProject/Assets/TestCinemachineScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TestCinemachineScript : MonoBehaviour
+{
+    public CinemachineVirtualCamera v1;
+    public CinemachineVirtualCamera v2;
+
+    bool toggle = false;
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            toggle = !toggle;
+            v1.enabled = toggle;
+            v2.enabled = !toggle;
+        }
+    }
+}
